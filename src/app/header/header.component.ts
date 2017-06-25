b28dc7cfebf3d7eabfe9525c507d00fa39614955
piry1 <piry3943@gmail.com>
2017-06-26 00:22:51 +0200

    kontakt i czcionka

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Question } from '../home/home.question';
 import { SharedService } from "../shared.service";
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-header',
@@ -13,23 +14,34 @@ export class HeaderComponent implements OnInit {
   private isScrollingDown = false;
   private hideNavbar = false;
   private bgNavbar = false;
+  private homeControler = false;
 
   appName = "Testownik";
 
   files: any = null;
-	isFileReaderWork: boolean = false;
-  counter: number=0;
-  fileName: string="Wybierz bazę"
-  err:boolean=false;
+  isFileReaderWork: boolean = false;
+  counter: number = 0;
+  fileName: string = "Wybierz bazę"
+  err: boolean = false;
 
 
-  constructor() {
+  constructor(private router: Router) {
     console.log("MyComp.constructor");
+    router.events.subscribe((val) => {
+      this.navStyle(val["url"]);
+    });
   }
 
 
-   fileChanged($event):void {
-    this.err=false;
+  navStyle(url) {
+    if (url == '/home')
+      this.homeControler = true;
+    else
+      this.homeControler = false;
+  }
+
+  fileChanged($event): void {
+    this.err = false;
     this.reset();
     this.files = (<HTMLInputElement>document.getElementById("file")).files;
 
@@ -38,33 +50,33 @@ export class HeaderComponent implements OnInit {
 
       var fileReader = new FileReader();
 
-      fileReader.onloadend= function(e){
-        if(<string>this.result.charAt(0)=='X'){
+      fileReader.onloadend = function (e) {
+        if (<string>this.result.charAt(0) == 'X') {
           SharedService.questions.push(new Question(this.result, self.counter));
           self.counter++;
-        }else{
-            console.log("Błędna baza")
-            self.err=true;
+        } else {
+          console.log("Błędna baza")
+          self.err = true;
         }
       }
-      if(self.err){
+      if (self.err) {
         break;
       }
       fileReader.readAsText(file, 'windows-1250');
     }
   }
 
-  reset(){
-    SharedService.questions=[];
-    SharedService.allCounter=0;
-    SharedService.goodCounter=0;
-    this.counter=0;
+  reset() {
+    SharedService.questions = [];
+    SharedService.allCounter = 0;
+    SharedService.goodCounter = 0;
+    this.counter = 0;
   }
 
-  randomSequence(){
-    let tmp=SharedService.shuffle(SharedService.questions)
-    this.reset();  
-    SharedService.questions=tmp;
+  randomSequence() {
+    let tmp = SharedService.shuffle(SharedService.questions)
+    this.reset();
+    SharedService.questions = tmp;
   }
 
   questions() {
