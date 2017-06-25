9213b55f6be66c314cbbe45fcff0932a08da23f2
piry1 <piry3943@gmail.com>
2017-06-25 23:11:38 +0200

    navbar

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -9,30 +9,35 @@ import { SharedService } from "../shared.service";
 })
 export class HeaderComponent implements OnInit {
 
+  private lastScroll = 0;
+  private isScrollingDown = false;
+  private hideNavbar = false;
+  private bgNavbar = false;
+
   appName = "Testownik";
 
   files: any = null;
-	isFileReaderWork: boolean = false;
-  counter: number=0;
-  fileName: string="Wybierz bazę"
+  isFileReaderWork: boolean = false;
+  counter: number = 0;
+  fileName: string = "Wybierz bazę"
 
-	constructor() {
-		console.log("MyComp.constructor");
+  constructor() {
+    console.log("MyComp.constructor");
   }
 
-   fileChanged($event):void {
-    SharedService.questions=[];
-    SharedService.allCounter=0;
-    SharedService.goodCounter=0;
-    this.counter=0
+  fileChanged($event): void {
+    SharedService.questions = [];
+    SharedService.allCounter = 0;
+    SharedService.goodCounter = 0;
+    this.counter = 0
     this.files = (<HTMLInputElement>document.getElementById("file")).files;
 
-    let self=this;
-    for(let file of this.files){
+    let self = this;
+    for (let file of this.files) {
 
-		  var fileReader = new FileReader();
+      var fileReader = new FileReader();
 
-      fileReader.onloadend= function(e){
+      fileReader.onloadend = function (e) {
         SharedService.questions.push(new Question(this.result, self.counter));
         self.counter++;
       }
@@ -53,6 +58,22 @@ export class HeaderComponent implements OnInit {
   }
 
   ngOnInit() {
+  }
+
+  shoulHideNavbar() {
+    if (this.lastScroll > 40 && this.isScrollingDown)
+      this.hideNavbar = true;
+    else
+      this.hideNavbar = false;
+  }
+
+  onScroll($event) {
+    var scroll = window.scrollY;
+    this.isScrollingDown = scroll > this.lastScroll ? true : false;
+    this.lastScroll = scroll;
+    this.shoulHideNavbar();
+
+    scroll > 80 ? this.bgNavbar = true : this.bgNavbar = false;
   }
 
 }
